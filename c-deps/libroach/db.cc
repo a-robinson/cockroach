8cfc0cec0aaeb1093d09ed4e7d7bd86df82b6f1b
Alex Robinson <alexdwanerobinson@gmail.com>
2017-11-08 15:06:34 +0000

    tweaks: disable low-pri, change rocksdb version, parallel writers

--- c-deps/libroach/db.cc
+++ c-deps/libroach/db.cc
@@ -1758,6 +1758,7 @@ DBStatus DBApproximateDiskBytes(DBEngine* db, DBKey start, DBKey end, uint64_t* 
 
 DBStatus DBImpl::Put(DBKey key, DBSlice value) {
   rocksdb::WriteOptions options;
+  //options.low_pri = true;
   options.disableWAL = true;
   return ToDBStatus(rep->Put(options, EncodeKey(key), ToSlice(value)));
 }
@@ -1780,6 +1781,7 @@ DBStatus DBPut(DBEngine* db, DBKey key, DBSlice value) { return db->Put(key, val
 
 DBStatus DBImpl::Merge(DBKey key, DBSlice value) {
   rocksdb::WriteOptions options;
+  //options.low_pri = true;
   options.disableWAL = true;
   return ToDBStatus(rep->Merge(options, EncodeKey(key), ToSlice(value)));
 }
@@ -1835,6 +1837,7 @@ DBStatus DBGet(DBEngine* db, DBKey key, DBString* value) { return db->Get(key, v
 
 DBStatus DBImpl::Delete(DBKey key) {
   rocksdb::WriteOptions options;
+  //options.low_pri = true;
   options.disableWAL = true;
   return ToDBStatus(rep->Delete(options, EncodeKey(key)));
 }
@@ -1855,6 +1858,7 @@ DBStatus DBSnapshot::Delete(DBKey key) { return FmtStatus("unsupported"); }
 
 DBStatus DBImpl::DeleteRange(DBKey start, DBKey end) {
   rocksdb::WriteOptions options;
+  //options.low_pri = true;
   options.disableWAL = true;
   return ToDBStatus(rep->DeleteRange(options, rep->DefaultColumnFamily(), EncodeKey(start), EncodeKey(end)));
 }
@@ -1898,6 +1902,7 @@ DBStatus DBBatch::CommitBatch(bool sync) {
     return kSuccess;
   }
   rocksdb::WriteOptions options;
+  //options.low_pri = true;
   options.disableWAL = true;
   options.sync = sync;
   return ToDBStatus(rep->Write(options, batch.GetWriteBatch()));
@@ -1908,6 +1913,7 @@ DBStatus DBWriteOnlyBatch::CommitBatch(bool sync) {
     return kSuccess;
   }
   rocksdb::WriteOptions options;
+  //options.low_pri = true;
   options.disableWAL = true;
   options.sync = sync;
   return ToDBStatus(rep->Write(options, &batch));
@@ -1926,6 +1932,7 @@ DBStatus DBCommitAndCloseBatch(DBEngine* db, bool sync) {
 DBStatus DBImpl::ApplyBatchRepr(DBSlice repr, bool sync) {
   rocksdb::WriteBatch batch(ToString(repr));
   rocksdb::WriteOptions options;
+  //options.low_pri = true;
   options.disableWAL = true;
   options.sync = sync;
   return ToDBStatus(rep->Write(options, &batch));
@@ -2100,6 +2107,7 @@ DBIterator* DBNewTimeBoundIter(DBEngine* db, DBTimestamp min_ts, DBTimestamp max
   const std::string max = EncodeTimestamp(max_ts);
   rocksdb::ReadOptions opts;
   opts.total_order_seek = true;
+  /*
   opts.table_filter = [min, max](const rocksdb::TableProperties& props) {
     auto userprops = props.user_collected_properties;
     auto tbl_min = userprops.find("crdb.ts.min");
@@ -2116,6 +2124,7 @@ DBIterator* DBNewTimeBoundIter(DBEngine* db, DBTimestamp min_ts, DBTimestamp max
     // exclusive, but the max_ts bound is inclusive.
     return max.compare(tbl_min->second) >= 0 && min.compare(tbl_max->second) < 0;
   };
+  */
   return db->NewIter(&opts);
 }
 
@@ -2509,10 +2518,13 @@ DBStatus DBSstFileWriterOpen(DBSstFileWriter* fw) {
 }
 
 DBStatus DBSstFileWriterAdd(DBSstFileWriter* fw, DBKey key, DBSlice val) {
+  /*
+  rocksdb::Status status = fw->rep.Add(EncodeKey(key), ToSlice(val));
   rocksdb::Status status = fw->rep.Put(EncodeKey(key), ToSlice(val));
   if (!status.ok()) {
     return ToDBStatus(status);
   }
+  */
   return kSuccess;
 }
 
